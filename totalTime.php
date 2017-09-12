79f21c9f895372df0c1dca429159fb7449f1fe5b
Alessio Bollea <alessio.bollea@live.it>
2017-09-12 17:50:02 +0200

    Correzione per leggera modifica al db

--- totalTime.php
+++ totalTime.php
@@ -3,7 +3,7 @@
 	
 	$usr = trim($_GET["user"]);
 	
-	$stmt = $conn->prepare("SELECT SUM(duration) FROM User,Session WHERE name=?");
+	$stmt = $conn->prepare("SELECT SUM(duration) FROM Session WHERE name=?");
 	$stmt->bind_param("s",$usr);
 	if($stmt->execute()) {
 		$stmt->bind_result($totalTime);
