923711dc6c828e7d6f1c9f78a39fadb5b8e062b5
Alessio Bollea <alessio.bollea@live.it>
2017-09-14 18:55:26 +0200

    Prima versione del file di upload sessioni

--- syncUp.php
+++ syncUp.php
@@ -1,5 +1,5 @@
 <?php
-	//formato json in input: {"data":[{"id":"1","duration":"30","year":"2017","month":"9","day":"13","th":"1","ex":"1","pr":"0","user":"Alessio","course":"Fisica"}, ... ]}
+	//formato json in input: {"data":[{"id":"1","duration":"30","year":"2017","month":"9","day":"13","th":"1","ex":"1","pr":"0","course":"Fisica"}, ... ],"user":"Bolle"}
 	
 	include 'dbInfo.php';
 	
@@ -7,6 +7,7 @@
 	$data = json_decode($json,true);
 	
 	$response;
+	$usr = trim($data['user']);
 	
 	$stmt = $conn->prepare("INSERT INTO Session (localId,duration,date,th,ex,pr,user,course) VALUES (?,?,?,?,?,?,?,?)");
 	
@@ -19,7 +20,6 @@
 		$th = trim($session['th']);
 		$ex = trim($session['ex']);
 		$pr = trim($session['pr']);
-		$usr = trim($session['user']);
 		$course = trim($session['course']);
 		
 		$stmt->bind_param("iisiiiss",$lastId,$duration,$date,$th,$ex,$pr,$usr,$course);
@@ -31,11 +31,11 @@
 	}
 	
 	if(!$fail)
-		$response = array("message" => "ok");
+		$response = array("message" => "ok","index" => $lastId);
 	
 	$stmt->close();
 	$conn->close();
 	print_r(json_encode($response,JSON_PRETTY_PRINT));
 	
-	//formato json in output: {"message":"ok"} o {"message":"error","index":"3"}
+	//formato json in output: {"message":"ok","index":"3"}
 ?>
